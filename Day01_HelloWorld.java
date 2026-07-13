40a9de9f7e528e04b6e002252adccd8e6b7069b0
Gayatri0428 <gayatrigudla447@gmail.com>
2026-07-13 20:28:35 +0530

    Add files via upload

---
+++ Day01_HelloWorld.java
@@ -0,0 +1,6 @@
+public class Day01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to my 365 Days of Java Journey!");
+    }
+}
