ef8f169bd529d0cef4fc52030320e04b3e706462
Gayatri0428 <gayatrigudla447@gmail.com>
2026-07-15 08:33:36 +0530

    Add files via upload

---
+++ Day02_VariablesAndDataTypes.java
@@ -0,0 +1,26 @@
+public class Day02_VariablesAndDataTypes {
+    public static void main(String[] args) {
+
+        // Integer
+        int age = 19;
+
+        // Floating-point number
+        double cgpa = 8.35;
+
+        // Character
+        char grade = 'A';
+
+        // Boolean
+        boolean isStudent = true;
+
+        // String
+        String name = "Gayatri";
+
+        // Display values
+        System.out.println("Name      : " + name);
+        System.out.println("Age       : " + age);
+        System.out.println("CGPA      : " + cgpa);
+        System.out.println("Grade     : " + grade);
+        System.out.println("Student   : " + isStudent);
+    }
+}
