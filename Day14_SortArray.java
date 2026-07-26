21eabbeb6e020e69423bd61cc9524f6452e7cf2d
Gayatri0428 <gayatrigudla447@gmail.com>
2026-07-26 22:00:58 +0530

    Add files via upload

---
+++ Day14_SortArray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Day14_SortArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] numbers = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nArray in Ascending Order:");
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        sc.close();
+    }
+}
