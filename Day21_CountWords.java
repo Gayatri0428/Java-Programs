30372ed94005a153afc7cf7580d6fae8b2fa0f73
Gayatri0428 <gayatrigudla447@gmail.com>
2026-08-03 10:14:32 +0530

    Add files via upload

---
+++ Day21_CountWords.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Day21_CountWords {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine().trim();
+
+        if (sentence.isEmpty()) {
+            System.out.println("Number of Words: 0");
+        } else {
+            String[] words = sentence.split("\\s+");
+            System.out.println("Number of Words: " + words.length);
+        }
+
+        sc.close();
+    }
+}
