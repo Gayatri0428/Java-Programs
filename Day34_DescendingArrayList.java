58ec683016ea895cbe85c76f0037a12d6019c464
Gayatri0428 <gayatrigudla447@gmail.com>
2026-08-21 16:21:12 +0530

    Add files via upload

---
+++ Day34_DescendingArrayList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Day34_DescendingArrayList {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter " + n + " numbers:");
+
+        for (int i = 0; i < n; i++) {
+            numbers.add(sc.nextInt());
+        }
+
+        System.out.println("\nOriginal ArrayList: " + numbers);
+
+        Collections.sort(numbers, Collections.reverseOrder());
+
+        System.out.println("Descending Order: " + numbers);
+
+        sc.close();
+    }
+}
